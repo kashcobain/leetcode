b5c017a9639b5f41ee11fe47f5527c17d0f271b2
kashcobain <karanshandilya19742001@gmail.com>
2025-08-23 05:49:47 +0530

    [204] [Time Beats: 60.03%] [Memory Beats: 66.81%] - LeetPush

--- Count-Primes.cpp
+++ Count-Primes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> p(n+1,true);
-        p[0]=p[1]=false;
-        int count=0;
-        for(int i=2;i<n;i++)
-        {
-            if(p[i]){
-                count++;
-                for(int j=i*2;j<n;j+=i)
-                {
-                    p[j]=false;
+        if(n <= 2) return 0;
+        
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for(int i = 2; i * i < n; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
-        return count;
+
+        return count(isPrime.begin(), isPrime.end(), true);
     }
 };
