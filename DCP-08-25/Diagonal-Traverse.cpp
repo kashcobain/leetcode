e58e1e5afea1511c6d5e7812a1fb7077851f97d4
kashcobain <karanshandilya19742001@gmail.com>
2025-08-25 07:36:17 +0530

    [498] [Time Beats: 60.72%] [Memory Beats: 91.64%] - LeetPush

--- DCP-08-25/Diagonal-Traverse.cpp
+++ DCP-08-25/Diagonal-Traverse.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        vector<int> ans;
-        map<int,vector<int>> mp;
-        int m=mat.size();
-        int n=mat[0].size();
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                mp[i+j].push_back(mat[i][j]);
+    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return {};
+
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> result(m * n);
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m * n; i++) {
+            result[i] = matrix[row][col];
+
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;
+                else if (row == 0) col++;
+                else { row--; col++; }
+            } else {
+                if (row == m - 1) col++;
+                else if (col == 0) row++;
+                else { row++; col--; }
             }
         }
-        for(auto [key,va]:mp)
-        {
-            if(key%2==0)
-            {
-                reverse(va.begin(),va.end());
-            }
-            for(auto i:va)
-            {
-                ans.push_back(i);
-            }
-        }
-        return ans;
+
+        return result;
     }
 };
