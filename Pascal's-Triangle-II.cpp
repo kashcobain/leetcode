fd9303cb8833ca4ec879d9ff4793cccefc691faa
kashcobain <karanshandilya19742001@gmail.com>
2025-09-25 18:55:36 +0530

    [119] [Time Beats: 100.00%] [Memory Beats: 37.80%] - LeetPush

---
+++ Pascal's-Triangle-II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        int n=rowIndex;
+        vector<vector<int>> ans;
+        for(int i=0;i<=n;i++)
+        {
+            vector<int> row(i+1,1);
+            for(int j=1;j<i;j++)
+            {
+                row[j]=ans[i-1][j-1]+ans[i-1][j];
+            }
+            ans.push_back(row);
+        }
+        return ans[n];
+        
+    }
+};
