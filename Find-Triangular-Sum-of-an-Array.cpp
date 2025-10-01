a6f835a733cc8ace607a708205c338fe78a3170b
kashcobain <karanshandilya19742001@gmail.com>
2025-10-01 12:57:59 +0530

    [2221] [Time Beats: 27.90%] [Memory Beats: 24.10%] - LeetPush

--- Find-Triangular-Sum-of-an-Array.cpp
+++ Find-Triangular-Sum-of-an-Array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> prev=nums;
-        for(int i=n;i>1;i--)
-        {
-            vector<int> curr(i-1);
-            for(int j=0;j<i-1;j++)
-            {
-                curr[j]=(prev[j]+prev[j+1])%10;
+        int n = nums.size();
+
+        // Step 1: base row dal do
+        vector<vector<int>> dp; 
+        dp.push_back(nums);   // dp[0] = original nums
+
+        // Step 2: har step pe ek row chhoti hoti jaayegi
+        for (int i = 1; i < n; i++) {
+            int size = n - i;
+            vector<int> row(size);
+
+            for (int j = 0; j < size; j++) {
+                row[j] = (dp[i-1][j] + dp[i-1][j+1]) % 10;
             }
-            prev=curr;
+
+            dp.push_back(row);
         }
-return prev[0];
+
+        // Step 3: last row me ek hi element hoga
+        return dp[n-1][0];
     }
 };
