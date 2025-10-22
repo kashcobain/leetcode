5fa2170c95060ba114b99a5a2c04ec73d96f3aa1
kashcobain <karanshandilya19742001@gmail.com>
2025-10-23 02:48:49 +0530

    [1047] [Time Beats: 67.70%] [Memory Beats: 16.12%] - LeetPush

---
+++ Remove-All-Adjacent-Duplicates-In-String.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        if (s.empty()) return "";
+
+        stack<char> st;
+        st.push(s[0]);
+
+        for (int i = 1; i < s.size(); i++) {
+            if (!st.empty() && st.top() == s[i])
+                st.pop();
+            else
+                st.push(s[i]);
+        }
+
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
