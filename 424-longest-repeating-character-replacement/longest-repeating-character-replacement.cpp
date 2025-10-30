5293ffbe3a96603326075c712f793e23e1029771
kashcobain <karanshandilya19742001@gmail.com>
2025-10-30 15:39:42 +0530

    Time: 8 ms (36.63%) | Memory: 11.1 MB (18.70%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,39 +1,20 @@
 class Solution {
 public:
-    
-    int maxfre(unordered_map<char,int> &mp) {
-        int maxi = 0;
-        for (auto it : mp) {
-            if (it.second > maxi) {
-                maxi = it.second;
-            }
-        }
-        return maxi;
-    }
-
     int characterReplacement(string s, int k) {
-        int n = s.size();
+        unordered_map<char, int> freq;
         int left = 0;
-        int maxi = 0;
-        unordered_map<char,int> mp;
-
-        for (int right = 0; right < n; right++) {
-            mp[s[right]]++;
-
-            int maxfreq = maxfre(mp);
-
-            
-            if (right - left + 1 - maxfreq <= k) {
-                int ans = right - left + 1;
-                maxi = max(ans, maxi);
-            } 
-            else {
-                
-                mp[s[left]]--;
+        int maxFreq = 0;
+        int res = 0;
+            for (int right = 0; right < s.size(); right++) {
+            freq[s[right]]++;
+            maxFreq = max(maxFreq, freq[s[right]]);  
+            while ((right - left + 1) - maxFreq > k) {
+                freq[s[left]]--;
                 left++;
             }
+            res = max(res, right - left + 1);
         }
 
-        return maxi;
+        return res;
     }
 };
