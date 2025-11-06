28d61d7ff2a099ba88728a7b2f927461b77085e5
kashcobain <karanshandilya19742001@gmail.com>
2025-11-07 00:56:46 +0530

    Time: 3 ms (96.44%) | Memory: 33 MB (60.63%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -1,44 +1,23 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
- TreeNode* helper(vector<int> &nums,int start,int end)
- {
-    if(start>end)return  nullptr;
-    int mid=start+(end-start)/2;
-    TreeNode* root=new TreeNode(nums[mid]);
-    root->left=helper(nums,start,mid-1);
-    root->right=helper(nums,mid+1,end);
-    return root;
- }
-    TreeNode* sortedListToBST(ListNode* head) {
-        vector<int> ans;
-        while(head!=nullptr)
-        {
-            ans.push_back(head->val);
-            head=head->next;
+    TreeNode* conv(ListNode* start, ListNode* end) {
+        if (start == end) return nullptr;
+
+        ListNode* slow = start;
+        ListNode* fast = start;
+        while (fast != end && fast->next != end) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        TreeNode* root=helper(ans,0,ans.size()-1);
+
+        TreeNode* root = new TreeNode(slow->val);
+        root->left = conv(start, slow);
+        root->right = conv(slow->next, end);
+
         return root;
-        
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        return conv(head, nullptr);
     }
 };
