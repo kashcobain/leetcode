528061b3075530b2b1467e452c487770acb57dcb
kashcobain <karanshandilya19742001@gmail.com>
2025-11-07 00:19:23 +0530

    Time: 1 ms (76.19%) | Memory: 23 MB (22.75%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -11,20 +11,19 @@
  */
 class Solution {
 public:
-    TreeNode* helper(vector<int> &nums, int start, int end)
-    {   
-        if (start > end) return nullptr;
-        
-        int middle = start + (end - start) / 2;
-        TreeNode* root = new TreeNode(nums[middle]);
-        
-        root->left = helper(nums, start, middle - 1);
-        root->right = helper(nums, middle + 1, end);
-        
+    TreeNode* helper(vector<int> &nums,int start,int end)
+    {   if(start>end)return nullptr;
+        int middle=start+(end-start)/2;
+        TreeNode* root=new TreeNode(nums[middle]);
+        root->left=helper(nums,start,middle-1);
+        root->right=helper(nums,middle+1,end);
         return root;
+        
     }
-
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return helper(nums, 0, nums.size() - 1);
+        return helper(nums,0,nums.size()-1);
+
+
+        
     }
 };
