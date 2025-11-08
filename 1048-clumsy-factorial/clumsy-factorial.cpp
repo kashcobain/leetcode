6fd45f8ab69b2d9efdd739ba40196d2486beba99
kashcobain <karanshandilya19742001@gmail.com>
2025-11-08 13:32:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (5.68%) - LeetSync

--- 1048-clumsy-factorial/clumsy-factorial.cpp
+++ 1048-clumsy-factorial/clumsy-factorial.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
+    int helper(int n, vector<int> &dp)
+    {
+        if(n==1)return 1;
+        if(n==2)return 2;
+        if(n==3)return 6;
+        if(n==4)return 6; 
+
+        if(dp[n]!=-1)return dp[n];
+
+        return dp[n] = (n*(n-1))/(n-2) + helper(n-4, dp);
+    }
+
     int clumsy(int n) {
-        if(n==1) return 1;
+        if(n==1)return 1;
         if(n==2)return 2;
         if(n==3)return 6;
         if(n==4)return 7;
-        if(n%4==1)return n+2;
-        if(n%4==2)return n+2;
-        if(n%4==3)return n-1;
-        return n+1;
 
+        vector<int> dp(10000, -1);
+        int i = n - 3;
+        int sum = 0;
 
-        
+        while(i >= 0)  
+        {
+            sum += i;
+            i -= 4;
+        }
+
+        return (n*(n-1))/(n-2) - helper(n-4, dp) + sum; 
     }
 };
