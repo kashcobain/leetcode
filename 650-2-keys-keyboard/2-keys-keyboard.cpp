c8cf78f0c5c6d7fd31e4ac479c4741b8c94d2d64
kashcobain <karanshandilya19742001@gmail.com>
2025-11-12 03:52:35 +0530

    Time: 30 ms (22.80%) | Memory: 89.1 MB (17.41%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+
     int minSteps(int n) {
         if(n==1)return 0;
-        return 1+minhelper(1,1,n);
+        vector<vector<int>> dp(n+1,vector<int>(n/2+1,-1));
+        return 1+minhelper(1,1,dp,n);
         
     }
-    int minhelper(int curlen,int pastlen,int n)
+    int minhelper(int curlen,int pastlen,vector<vector<int>> &dp,int n)
     {
         if(curlen==n)return 0;
         if(curlen>n)return 1000;
-        int op1=2+minhelper(curlen*2,curlen,n);
-        int op2=1+minhelper(curlen+pastlen,pastlen,n);
-        return min(op1,op2);
+        if(dp[curlen][pastlen]!=-1)return dp[curlen][pastlen];
+        int op1=1+minhelper(curlen+pastlen,pastlen,dp,n);
+        int op2=2+minhelper(curlen*2,curlen,dp,n);
+        dp[curlen][pastlen]=min(op1,op2);
+        return dp[curlen][pastlen];
     }
 };
