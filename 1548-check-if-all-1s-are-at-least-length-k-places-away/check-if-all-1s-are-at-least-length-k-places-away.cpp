1544a1d8ade3936dbbfcffd8ad1d34c321510b40
kashcobain <karanshandilya19742001@gmail.com>
2025-11-19 01:39:42 +0530

    Time: 2 ms (18.11%) | Memory: 61.4 MB (43.21%) - LeetSync

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int last=-1e9;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            {
+                if(i-last-1<k)return false;
+                
+                last=i;
+            }
+        }
+        return true;
+
+    }
+};
