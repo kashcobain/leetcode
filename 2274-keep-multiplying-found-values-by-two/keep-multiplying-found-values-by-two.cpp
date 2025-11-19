eba88781646f306a3e513d851e30f19b046172db
kashcobain <karanshandilya19742001@gmail.com>
2025-11-19 17:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (68.93%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        while(find(nums.begin(),nums.end(),original)!=nums.end())
-        {
-            original=original*2;
+        sort(nums.begin(), nums.end());
+        for (int num : nums) {
+            if (original == num) {
+                original *= 2;
+            }
         }
-return original;
-}
+        return original;
+    }
 };
+// class Solution {
+// public:
+//     int findFinalValue(vector<int>& nums, int original) {
+//         unordered_set<int> s(nums.begin(), nums.end());
+//         while (s.count(original)) {
+//             original *= 2;
+//         }
+//         return original;
+//     }
+// };
