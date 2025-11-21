3e98edc262d4cdbaad3e1f802de5c2d07e57c4ae
kashcobain <karanshandilya19742001@gmail.com>
2025-11-21 23:59:31 +0530

    Time: 20 ms (51.56%) | Memory: 88.5 MB (7.65%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
-        string res = "";
-        int j = 0; // last cut position in s
-
-        for (int i = 0; i < spaces.size(); i++) {
-            int cut = spaces[i];            // yahan space lagana hai
-
-            // j se leke cut-1 tak ka substring uthao
-            res += s.substr(j, cut - j);
-
-            // yahan space add karo
-            res += " ";
-
-            // ab next substring yahin se start hoga
-            j = cut;
+        string res="";
+        int j=0;
+        for(int i=0;i<spaces.size();i++)
+        {
+            int cut=spaces[i];
+            res+=s.substr(j,cut-j);
+        res+=" ";
+        j=cut;
         }
-
-        // last bacha hua part bhi add karna zaruri hai
-        res += s.substr(j);
-
-        return res;
+        return res+=s.substr(j);
     }
 };
