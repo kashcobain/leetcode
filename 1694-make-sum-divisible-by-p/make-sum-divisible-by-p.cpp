898d73a8c67cc57d29f211f71a395f48153af832
kashcobain <karanshandilya19742001@gmail.com>
2025-11-30 17:23:36 +0530

    Time: 74 ms (75.00%) | Memory: 88 MB (35.35%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,38 +1,25 @@
 class Solution {
-public:
-    int minSubarray(vector<int>& nums, int p) {
+ public:
+  int minSubarray(vector<int>& nums, int p) {
+    const long sum = accumulate(nums.begin(), nums.end(), 0L);
+    const int remainder = sum % p;
+    if (remainder == 0)
+      return 0;
 
-        long long sum = 0;
-        for (auto x : nums) sum += x;
+    unordered_map<int, int> prefixToIndex{{0, -1}};
+    int ans = nums.size();
+    int prefix = 0;
 
-        int rem = sum % p;
-        if (rem == 0) return 0;
-
-        unordered_map<int, int> mp;
-        mp[0] = 0;
-
-        int n = nums.size();
-        vector<int> pre(n + 1, 0);
-
-        for (int i = 1; i <= n; i++) {
-            pre[i] = (pre[i - 1] + nums[i - 1]) % p;
-        }
-
-        int ans = n;
-
-        for (int i = 1; i <= n; i++) {
-
-            int curr = pre[i];
-
-            int need = (curr - rem + p) % p;
-
-            if (mp.count(need)) {
-                ans = min(ans, i - mp[need]);
-            }
-
-            mp[curr] = i;
-        }
-
-        return ans == n ? -1 : ans;
+    for (int i = 0; i < nums.size(); ++i) {
+      prefix += nums[i];
+      prefix %= p;
+      const int target = (prefix - remainder + p) % p;
+      if (const auto it = prefixToIndex.find(target);
+          it != prefixToIndex.cend())
+        ans = min(ans, i - it->second);
+      prefixToIndex[prefix] = i;
     }
+
+    return ans == nums.size() ? -1 : ans;
+  }
 };
