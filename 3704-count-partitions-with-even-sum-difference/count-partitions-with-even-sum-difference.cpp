e2f4d360f9b71ad6842c314ad88aa0e7b0e1839c
kashcobain <karanshandilya19742001@gmail.com>
2025-12-05 13:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (53.79%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.cpp
@@ -6,17 +6,6 @@ public:
         {
             sum+=nums[i];
         }
-        int count=0;
-        int sum1=0;
-        for(int i=0;i<nums.size()-1;i++)
-        {
-           sum1+=nums[i];
-           sum-=nums[i];
-           if((sum-sum1)%2==0)
-           {
-            count++;
-           }
-        }
-        return count;
+        return sum%2==0?nums.size()-1:0;
     }
 };
