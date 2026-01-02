6c8de0ec2accd73790d3a8ac7c90042e2afe1ab4
kashcobain <karanshandilya19742001@gmail.com>
2026-01-02 16:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.6 MB (42.84%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find(nums[i])!=mp.end())
+            {
+                return nums[i];
+            }
+            else{
+                mp[nums[i]]++;
+            }
+        }
+
+        return -1;
+    }
+};
