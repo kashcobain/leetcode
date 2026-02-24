fc8d25fe86fb1acf0dee07b864665566f78a1507
kashcobain <karanshandilya19742001@gmail.com>
2026-02-25 02:33:02 +0530

    Time: 2079 ms (6.65%) | Memory: 7.8 MB (89.79%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n <= 1) return 1;
+
+        int ans = 0;
+
+        for(int i = 1; i <= n; i++) {
+            int left = numTrees(i - 1);
+            int right = numTrees(n - i);
+            ans += left * right;
+        }
+
+        return ans;
+    }
+};
