3380b54812aa2ae7a2e7621356f6e6e5dca5d514
kashcobain <karanshandilya19742001@gmail.com>
2026-02-25 18:06:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (5.50%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void sum(TreeNode* root, vector<string> &ans, string &temp)
+    {
+        if(root == nullptr)
+            return;
+
+        // current node value add karo
+        temp += to_string(root->val);
+
+        // agar leaf node hai to store karo
+        if(root->left == nullptr && root->right == nullptr) {
+            ans.push_back(temp);
+        }
+        else {
+            sum(root->left, ans, temp);
+            sum(root->right, ans, temp);
+        }
+
+        temp.pop_back();
+    }
+
+    int sumNumbers(TreeNode* root) {
+        vector<string> ans;
+        string temp = "";
+        sum(root, ans, temp);
+
+        int res = 0;
+        for(int i = 0; i < ans.size(); i++) {
+            res += stoi(ans[i]);
+        }
+
+        return res;
+    }
+};
