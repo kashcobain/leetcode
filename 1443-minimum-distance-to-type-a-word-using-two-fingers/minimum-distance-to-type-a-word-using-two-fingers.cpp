64bfa354142d41721ca7236f05e8ed1c6e746e52
kashcobain <karanshandilya19742001@gmail.com>
2026-04-13 15:10:49 +0530

    Time: 87 ms (17.51%) | Memory: 48.7 MB (20.23%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    int dp[301][27][27];
-
     int dist(int a, int b) {
-        if (a == 26) return 0;
+        if(a == 26) return 0;
         int x1 = a / 6, y1 = a % 6;
         int x2 = b / 6, y2 = b % 6;
         return abs(x1 - x2) + abs(y1 - y2);
     }
 
-    int solve(string &word, int i, int f1, int f2) {
-        if (i == word.size()) return 0;
-
-        if (dp[i][f1][f2] != -1) return dp[i][f1][f2];
-
-        int curr = word[i] - 'A';
-
-        int op1 = dist(f1, curr) + solve(word, i+1, curr, f2);
-
-        int op2 = dist(f2, curr) + solve(word, i+1, f1, curr);
-
-        return dp[i][f1][f2] = min(op1, op2);
-    }
-
     int minimumDistance(string word) {
-        memset(dp, -1, sizeof(dp));
-        return solve(word, 0, 26, 26);
+        int n = word.size();
+
+        vector<vector<vector<int>>> dp(n+1,
+            vector<vector<int>>(27, vector<int>(27, 0)));
+
+    
+
+        for(int i = n-1; i >= 0; i--) {
+            int curr = word[i] - 'A';
+
+            for(int f1 = 0; f1 <= 26; f1++) {
+                for(int f2 = 0; f2 <= 26; f2++) {
+
+                    int useF1 = dist(f1, curr) + dp[i+1][curr][f2];
+                    int useF2 = dist(f2, curr) + dp[i+1][f1][curr];
+
+                    dp[i][f1][f2] = min(useF1, useF2);
+                }
+            }
+        }
+
+        return dp[0][26][26];
     }
 };
