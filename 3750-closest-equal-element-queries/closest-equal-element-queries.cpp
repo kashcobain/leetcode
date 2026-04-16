2ca147f5d80a4a4272f5c4f6103341b2d86dd7d0
kashcobain <karanshandilya19742001@gmail.com>
2026-04-17 01:11:37 +0530

    Time: 214 ms (63.82%) | Memory: 216.8 MB (44.72%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -2,45 +2,44 @@ class Solution {
 public:
     vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
         int n = nums.size();
-        unordered_map<int,vector<int>> mp;
-        vector<int> ans;
 
+        unordered_map<int, vector<int>> mp;
+
+ 
         for(int i = 0; i < n; i++) {
             mp[nums[i]].push_back(i);
         }
 
-        for(auto q : queries) {
+        vector<int> ans;
+
+        for(int q : queries) {
+
             auto &vec = mp[nums[q]];
 
+         
             if(vec.size() == 1) {
                 ans.push_back(-1);
                 continue;
             }
 
             auto it = lower_bound(vec.begin(), vec.end(), q);
+
             int mini = INT_MAX;
 
-           
             int idx;
-            if(it == vec.end()) {
+            if(it + 1 != vec.end())
+                idx = *(it + 1);
+            else
                 idx = vec[0];
-            } else {
-                idx = *it;
-                if(idx == q) {
-                    if(it + 1 != vec.end()) idx = *(it + 1);
-                    else idx = vec[0];
-                }
-            }
 
             int d = min((idx - q + n) % n, (q - idx + n) % n);
             mini = min(mini, d);
 
-          
-            if(it != vec.begin()) {
+       
+            if(it != vec.begin())
                 idx = *(it - 1);
-            } else {
+            else
                 idx = vec.back();
-            }
 
             d = min((idx - q + n) % n, (q - idx + n) % n);
             mini = min(mini, d);
