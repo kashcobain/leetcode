ebd7ea3d41d4d65fe5b01b66dd5e29009ddfc755
kashcobain <karanshandilya19742001@gmail.com>
2026-04-17 00:56:59 +0530

    Time: 207 ms (70.15%) | Memory: 216.8 MB (49.25%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        int n = nums.size();
+        unordered_map<int,vector<int>> mp;
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        for(auto q : queries) {
+            auto &vec = mp[nums[q]];
+
+            if(vec.size() == 1) {
+                ans.push_back(-1);
+                continue;
+            }
+
+            auto it = lower_bound(vec.begin(), vec.end(), q);
+            int mini = INT_MAX;
+
+           
+            int idx;
+            if(it == vec.end()) {
+                idx = vec[0];
+            } else {
+                idx = *it;
+                if(idx == q) {
+                    if(it + 1 != vec.end()) idx = *(it + 1);
+                    else idx = vec[0];
+                }
+            }
+
+            int d = min((idx - q + n) % n, (q - idx + n) % n);
+            mini = min(mini, d);
+
+          
+            if(it != vec.begin()) {
+                idx = *(it - 1);
+            } else {
+                idx = vec.back();
+            }
+
+            d = min((idx - q + n) % n, (q - idx + n) % n);
+            mini = min(mini, d);
+
+            ans.push_back(mini);
+        }
+
+        return ans;
+    }
+};
