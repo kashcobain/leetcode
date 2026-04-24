e36b6d1f07ff3871cf9cfbb6adb469c025eca321
kashcobain <karanshandilya19742001@gmail.com>
2026-04-24 23:02:34 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (80.13%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=n-1;
+        int ans=0;
+        while(i<j)
+        {
+            ans+=nums[j]-nums[i];
+            i++;
+            j--;
 
-        vector<int> pre(n);
-        pre[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i-1] + nums[i];
+
+
         }
-
-        int mid = n / 2;
-
-        int leftsum = (mid > 0) ? pre[mid - 1] : 0;
-        int rightsum = pre[n - 1] - pre[mid];
-
-        int leftcount = mid;
-        int rightcount = n - mid - 1;
-
-        int leftval = nums[mid] * leftcount - leftsum;
-        int rightval = rightsum - nums[mid] * rightcount;
-
-        return leftval + rightval;
+        return ans;
+        
     }
 };
