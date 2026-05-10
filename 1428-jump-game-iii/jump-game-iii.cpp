d1de49d14280363cb4e754352001d7856bbd7903
kashcobain <karanshandilya19742001@gmail.com>
2026-05-10 18:15:47 +0530

    Time: 1 ms (56.28%) | Memory: 36.7 MB (59.40%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    bool solve(int ind,vector<int>& arr,vector<bool> &vis)
-    {
-        int n=arr.size();
-        if(arr[ind]==0)return true;
-        bool c1=false;
-        bool c2=false;
-        if(vis[ind])return false;
-        vis[ind]=true;
-        if(ind+arr[ind]<n) c1=solve(ind+arr[ind],arr,vis);
-        if(ind-arr[ind]>=0) c2=solve(ind-arr[ind],arr,vis);
-        return c1||c2;
-
-    }
     bool canReach(vector<int>& arr, int start) {
         int n=arr.size();
-        vector<bool> vis(n, false);
-        return solve(start,arr,vis);
-        
+        queue<int> q;
+        vector<int> vis(n,0);
+        q.push(start);
+        vis[start]=1;
+        while(!q.empty())
+        {
+            int ind=q.front();
+            q.pop();
+            if(arr[ind]==0)
+            {
+                return true;
+            }
+            int fwd=ind+arr[ind];
+            int bwd=ind-arr[ind];
+            if(fwd <n && vis[fwd]==0)
+            {
+                q.push(fwd);
+                vis[fwd]=1;
+            }
+            if(bwd>=0 && vis[bwd]==0)
+            {
+                q.push(bwd);
+                vis[bwd]=1;
+            }
+
+        }
+        return false;
     }
 };
