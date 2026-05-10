4ba0569320ab887aec1aae7e18430ae2ff52841a
kashcobain <karanshandilya19742001@gmail.com>
2026-05-10 16:02:57 +0530

    Time: 213 ms (6.75%) | Memory: 22.6 MB (6.52%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int ind,vector<int>& nums,vector<int> &dp)
+    {
+        int n=nums.size();
+        if(ind==n-1)return 0;
+        int ans=INT_MAX;
+        if(dp[ind]!=-1)return dp[ind];
+        for(int j=ind+1;j<min(ind+nums[ind]+1,n);j++)
+        {
+            int jump=solve(j,nums,dp);
+            if(jump!=INT_MAX)
+            {
+                ans=min(ans,1+jump);
+
+            }
+            
+    
+        }
+        return dp[ind]=ans;
+    }
+    int jump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return solve(0,nums,dp);
+
+        
+    }
+};
