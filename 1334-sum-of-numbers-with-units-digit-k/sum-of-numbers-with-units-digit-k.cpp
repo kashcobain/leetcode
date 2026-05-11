399317213d91eb0c4ec31c5df012914f19715b7a
kashcobain <karanshandilya19742001@gmail.com>
2026-05-11 15:16:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (81.55%) - LeetSync

--- 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.cpp
+++ 1334-sum-of-numbers-with-units-digit-k/sum-of-numbers-with-units-digit-k.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
-
     int minimumNumbers(int num, int k) {
-
-        if(num == 0)
-            return 0;
-
-        vector<int> coins;
-        for(int i = k; i <= num; i += 10)
+        if(num==0)return 0;
+        for(int i=1;i<=10;i++)
         {
-            coins.push_back(i);
-        }
-        vector<int> dp(num + 1, 1e9);
-        dp[0] = 0;
-        for(int coin : coins)
-        {
-            for(int target = coin;
-                target <= num;
-                target++)
+            if((i*k)%10==num%10 && i*k<=num)
             {
-                dp[target] =
-                    min(dp[target],
-                        1 + dp[target - coin]);
+                return i;
             }
         }
-
-        return dp[num] == 1e9
-               ? -1
-               : dp[num];
+return -1;
+        
     }
 };
