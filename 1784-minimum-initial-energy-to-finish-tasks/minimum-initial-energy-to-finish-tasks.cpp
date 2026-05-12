bf47c72b203c3481515380203985f46f8da1fe60
kashcobain <karanshandilya19742001@gmail.com>
2026-05-12 14:03:53 +0530

    Time: 1600 ms (8.74%) | Memory: 481.6 MB (7.28%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -1,35 +1,61 @@
 class Solution {
 public:
-    struct cmp{
-        bool operator()(const vector<int>& a,const vector<int>& b)const
-        {
-            if(a[1]-a[0]==b[1]-b[0])
-            {
-                return a<b;
-            }
-            return a[1]-a[0]>b[1]-b[0];
 
+    bool pos(vector<vector<int>>& tasks, int energy)
+    {
+        for(auto task : tasks)
+        {
+            int req = task[1];
+            int used = task[0];
+
+            if(energy < req)
+            {
+                return false;
+            }
+
+            energy = energy - used;
         }
 
+        return true;
+    }
+
+    struct cmp
+    {
+        bool operator()(const vector<int>& a,
+                        const vector<int>& b) const
+        {
+            if(a[1]-a[0] == b[1]-b[0])
+            {
+                return a < b;
+            }
+
+            return a[1]-a[0] > b[1]-b[0];
+        }
     };
+
     int minimumEffort(vector<vector<int>>& tasks) {
-        int n=tasks.size();
-        sort(tasks.begin(),tasks.end(),cmp());
-        int ans=0;
-        int curr=0;
-        for(auto &task:tasks)
+
+        sort(tasks.begin(), tasks.end(), cmp());
+
+        int low = 1;
+        int high = 1e9+7;
+
+        int ans = 0;
+
+        while(low <= high)
         {
-            int actual=task[0];
-            int mini=task[1];
-            if(curr<mini)
+            int mid = low + (high-low)/2;
+
+            if(pos(tasks, mid))
             {
-                ans+=mini-curr;
-                curr=mini;
+                ans = mid;
+                high = mid - 1;
             }
-            curr-=actual;
+            else
+            {
+                low = mid + 1;
+            }
         }
-
-
 
         return ans;
     }
