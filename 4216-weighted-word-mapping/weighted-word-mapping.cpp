343ee69236ced8860321dab49e946706b1fd1428
kashcobain <karanshandilya19742001@gmail.com>
2026-06-13 21:53:43 +0530

    Time: 3 ms (27.99%) | Memory: 43.6 MB (77.60%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (string &word : words) {
+            int wt = 0;
+
+            for (char ch : word) {
+                wt += weights[ch - 'a'];
+            }
+
+            wt %= 26;
+
+            ans += char('z' - wt);
+        }
+
+        return ans;
+    }
+};
