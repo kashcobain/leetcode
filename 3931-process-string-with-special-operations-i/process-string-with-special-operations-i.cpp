d9e55b95bd5048a5b6e378c2cf5170a835e89a68
kashcobain <karanshandilya19742001@gmail.com>
2026-06-16 10:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (42.05%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+
+        for(char ch : s) {
+            if(ch == '*') {
+                if(!ans.empty())
+                    ans.pop_back();
+            }
+            else if(ch == '#') {
+                ans += ans;
+            }
+            else if(ch == '%') {
+                reverse(ans.begin(), ans.end());
+            }
+            else {
+                ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};
