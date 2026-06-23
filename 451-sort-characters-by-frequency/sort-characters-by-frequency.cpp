40a8a849eb014cd8cbabbdbe47493ecc36c085de
kashcobain <karanshandilya19742001@gmail.com>
2026-06-23 21:57:19 +0530

    Time: 1 ms (68.49%) | Memory: 11.7 MB (22.48%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(const pair<int,char>& a,
+                        const pair<int,char>& b) const {
+            return a.first > b.first;
+        }
+    };
+
+    string frequencySort(string s) {
+
+        unordered_map<char,int> mp;
+
+        for(char c : s)
+        {
+            mp[c ]++;
+        }
+
+        vector<pair<int,char>> v;
+
+        for(auto it:mp)
+        {
+           
+                v.push_back({it.second, it.first});
+     
+        }
+
+        sort(v.rbegin(), v.rend());   
+
+        string ans = "";
+
+        for(auto it : v)
+        {
+            ans += string(it.first, it.second);
+        }
+
+        return ans;
+    }
+};
