8ad854617d6fd2966633b0f521c41ab630de2958
kashcobain <karanshandilya19742001@gmail.com>
2026-06-27 12:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (2.89%) - LeetSync

---
+++ 2278-find-three-consecutive-integers-that-sum-to-a-given-number/find-three-consecutive-integers-that-sum-to-a-given-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<long long> sumOfThree(long long num) {
+        vector<long long> ans;
+        if(num%3!=0)return {};
+        else{
+            ans.push_back(num/3-1);
+            ans.push_back(num/3);
+            ans.push_back(num/3+1);
+        }
+return ans;
+        
+    }
+};
