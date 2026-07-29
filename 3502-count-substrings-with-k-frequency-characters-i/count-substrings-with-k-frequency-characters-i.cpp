615772ab9bbc11b7a3aefa976746be27bd4c0172
kashcobain <karanshandilya19742001@gmail.com>
2026-07-29 19:32:56 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (90.64%) - LeetSync

--- 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.cpp
+++ 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.cpp
@@ -9,7 +9,7 @@ public:
         for(int right=0;right<n;right++ )
         {
             mp[s[right]-'a']++;
-            while(mp[s[right]-'a']>=k)
+            while(mp[s[right]-'a']==k)
             {
                 ans+=n-right;
                 mp[s[left]-'a']--;
