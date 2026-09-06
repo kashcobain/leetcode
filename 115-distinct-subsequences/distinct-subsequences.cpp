68fe27b388eadd446b1f593818d341a9137f909a
kashcobain <karanshandilya19742001@gmail.com>
2026-09-06 12:18:36 +0530

    Time: 27 ms (71.17%) | Memory: 26.9 MB (64.23%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1,0));
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0]=1;
-            
-        }
-        for(int j=1;j<=m;j++)
-        {
-            dp[0][j]=0;
+        int n = s.size();
+        int m = t.size();
 
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int MOD = (int) 1e9 + 7; 
+
+        for(int i = 0; i <= n; i++){
+            dp[i][m] = 1;
         }
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s[i-1]==t[j-1])
-                {
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
+
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                dp[i][j] = dp[i + 1][j];
+
+                if(s[i] == t[j]){
+                    dp[i][j] =  (dp[i][j] + dp[i + 1][j + 1]) % MOD;
                 }
             }
         }
-        return dp[n][m];
+
+        return dp[0][0];
     }
 };
